60b56af58e718d1c4184fd0ac9d694d4629b032b
Yurii <ext.yurii.troniak@swarco.com>
2024-02-12 13:49:17 +0200

    1 hour reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { interval, map, switchMap } from 'rxjs';
+import { interval, map, switchMap, timer } from 'rxjs';
 import { RxLet } from '@rx-angular/template/let';
 import { CommonModule } from '@angular/common';
 import { environment } from '../environments/environment';
@@ -24,6 +24,7 @@ interface IResponse {
   styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [CommonModule, HttpClientModule, RxLet],
+  host: { class: 'h-full w-full' },
 })
 export class AppComponent {
   title = 'mono-banka';
@@ -31,14 +32,27 @@ export class AppComponent {
   JAR_KEY = environment.JAR_KEY;
   link = `https://api.monobank.ua/bank/jar/HfSCkWGoDun87SYGstsK9q594bTVYp`;
 
-  req$ = this.http.get<IResponse>(this.link).pipe(
-    map((res) => {
-      console.log(res);
-      return {
-        ...res,
-        amount: res.amount.toString().slice(0, -2),
-        goal: res.goal.toString().slice(0, -2),
-      };
-    })
+  req$ = timer(0, 1 * 60 * 1000).pipe(
+    switchMap(() =>
+      this.http.get<IResponse>(this.link).pipe(
+        map((res) => {
+          console.log(res);
+          return {
+            ...res,
+            amount: res.amount.toString().slice(0, -2),
+            goal: res.goal.toString().slice(0, -2),
+          };
+        })
+      )
+    )
   );
+
+  constructor() {
+    interval(60 * 60 * 1000).pipe(
+      map(() => {
+        console.log('reloading');
+        window.location.reload();
+      })
+    ).subscribe();
+  }
 }
