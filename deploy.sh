158b7bdcd8b661b0508f491459117672a4d05ea3
Yurii <ext.yurii.troniak@swarco.com>
2024-02-13 00:49:14 +0200

    script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build Angular project
-ng build --configuration production --base-href /tronya.github.io/
-
-# Deploy to GitHub Pages
-npx angular-cli-ghpages --dir=dist/tronya.github.io/
