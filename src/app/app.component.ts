496bb05c1d219d6efa30066b09a426b4c20329c6
Yurii <ext.yurii.troniak@swarco.com>
2024-03-04 15:47:09 +0200

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { interval, map, of, switchMap, timer } from 'rxjs';
+import {
+  concatMap,
+  delay,
+  distinctUntilChanged,
+  interval,
+  map,
+  of,
+  switchMap,
+  timer,
+} from 'rxjs';
 import { RxLet } from '@rx-angular/template/let';
 import { CommonModule } from '@angular/common';
 import { environment } from '../environments/environment';
 import { CharacterComponent } from './character/character.component';
 import { DigitsComponent } from './digits/digits.component';
 import { PlaneComponent } from './plane.component';
-import { CanvaComponent } from './canva/canva.component';
+import { ParticlesComponent } from './particles/particles.component';
 
 interface IResponse {
   amount: number;
@@ -34,7 +43,7 @@ interface IResponse {
     CharacterComponent,
     DigitsComponent,
     PlaneComponent,
-    CanvaComponent,
+    ParticlesComponent,
   ],
   host: { class: 'h-full w-full bg-black' },
   encapsulation: ViewEncapsulation.None,
@@ -45,29 +54,31 @@ export class AppComponent {
   JAR_KEY = environment.JAR_KEY;
   link = `https://api.monobank.ua/bank/jar/HfSCkWGoDun87SYGstsK9q594bTVYp`;
 
-  req$ =
-    // of({
-    //   description: 'soo',
-    //   amount: '13232',
-    //   goal: '24334',
-    //   title: 'sraka',
-    // });
+  // req$ = of(1, 5, 2, 2, 3, 4, 4, 4, 5, 5).pipe(
+  //   concatMap((number) => of(number).pipe(delay(2000))),
+  //   distinctUntilChanged(),
+  //   map((value) => {
+  //     console.log(value);
+  //     return { amount: `magic` };
+  //   })
+  // );
 
-    timer(0, 1 * 60 * 1000).pipe(
-      switchMap(() =>
-        this.http.get<IResponse>(this.link).pipe(
-          map((res) => {
-            console.log(res);
+  req$ = timer(0, 1 * 30 * 1000).pipe(
+    switchMap(() =>
+      this.http.get<IResponse>(this.link).pipe(
+        map((res) => {
+          console.log(res);
 
-            return {
-              ...res,
-              amount: res.amount.toString().slice(0, -2),
-              goal: res.goal.toString().slice(0, -2),
-            };
-          })
-        )
+          return {
+            ...res,
+            amount: res.amount.toString().slice(0, -2),
+            goal: res.goal.toString().slice(0, -2),
+          };
+        })
       )
-    );
+    ),
+    distinctUntilChanged()
+  );
 
   constructor() {
     interval(60 * 60 * 1000)
---
+++ src/app/particles/particles.helper.ts
@@ -0,0 +1,60 @@
+const colors = ['#468966', '#FFF0A5', '#FFB03B', '#B64926', '#8E2800'];
+export class Particle {
+  x: number;
+  y: number;
+  dest: { x: number; y: number };
+  r: number;
+  vx: number;
+  vy: number;
+  accX: number;
+  accY: number;
+  friction: number;
+  color: string;
+  radius: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    this.dest = {
+      x: x,
+      y: y,
+    };
+    this.r = Math.random() * 5 + 2;
+    this.vx = (Math.random() - 0.5) * 20;
+    this.vy = (Math.random() - 0.5) * 20;
+    this.accX = 0;
+    this.accY = 0;
+    this.friction = Math.random() * 0.05 + 0.94;
+    this.radius = 1;
+
+    this.color = colors[Math.floor(Math.random() * 6)];
+  }
+
+  render(ctx: CanvasRenderingContext2D): void {
+    this.accX = (this.dest.x - this.x) / 1000;
+    this.accY = (this.dest.y - this.y) / 1000;
+    this.vx += this.accX;
+    this.vy += this.accY;
+    this.vx *= this.friction;
+    this.vy *= this.friction;
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false); // Corrected arguments
+    ctx.fill();
+
+    const a = this.x;
+    const b = this.y;
+
+    const distance = Math.sqrt(a * a + b * b);
+    if (distance < this.radius * 70) {
+      this.accX = this.x / 100;
+      this.accY = this.y / 100;
+      this.vx += this.accX;
+      this.vy += this.accY;
+    }
+  }
+}
