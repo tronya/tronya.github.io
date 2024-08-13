665d410840c833789b16ff9aa3ac021dade8adc9
Yurii <ext.yurii.troniak@swarco.com>
2024-08-13 12:21:22 +0300

    changes

--- src/api/monobank.ts
+++ src/api/monobank.ts
@@ -2,10 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { timer, switchMap, map, distinctUntilChanged } from 'rxjs';
 import { IResponse } from '../app/components/dots-text/particles/particles.helper';
-import { environment } from '../environments/environment';
-import { routes } from '../app/app.routes';
 
-const JAR_KEY = environment.JAR_KEY;
 const host = 'https://api.monobank.ua';
 const jar = 'HfSCkWGoDun87SYGstsK9q594bTVYp';
 const jarRoute = '/bank/jar/';
